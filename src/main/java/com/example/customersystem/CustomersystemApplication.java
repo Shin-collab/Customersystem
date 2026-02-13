f61379d7b465cab6d1869af21ffd3379e4619288
Shin-collab <sskg82760@gmail.com>
2026-02-13 19:13:51 +0700

    Update CustomersystemApplication.java

--- src/main/java/com/example/customersystem/CustomersystemApplication.java
+++ src/main/java/com/example/customersystem/CustomersystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomersystemApplication {
 	
 	public static void main(String[] args) {
+		// รันโปรเจกต์ Customer Management System
 		SpringApplication.run(CustomersystemApplication.class, args);
 	}
 
