dcb3a8b985b8a783216dd12acd222789fa23b954
Shin-collab <sskg82760@gmail.com>
2026-02-13 19:06:41 +0700

    Update UserRepository.java

--- src/main/java/com/example/customersystem/repository/UserRepository.java
+++ src/main/java/com/example/customersystem/repository/UserRepository.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // สำหรับเช็คตอน Login
     User findByUsername(String username);
 
+    // สำหรับเช็คตอนสมัครสมาชิก หรือลืมรหัสผ่าน
     User findByEmail(String email);
     
+    // สำหรับระบบ Reset Password
     User findByResetPasswordToken(String token);
 }
